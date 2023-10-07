139a9d70821570d7a50764112326288c5c40c977
Vikrant <06learntocode11@gmial.com>
2023-10-07 23:57:55 +0530

    Fandom Anime quiz

---
+++ index.js
@@ -0,0 +1,115 @@
+const rs = require('readline-sync');
+const chalk = require('chalk');
+
+// track the user score
+let score = 0;
+const wrongAnswer = chalk.bold.red;
+const rightAnswer = chalk.bold.green;
+// const warning = chalk.hex('#FFA500');
+
+// top scorers
+let topScorers = [
+    {
+        name: "Vikrant",
+        score: 45
+    },
+    {
+        name: "Harleen",
+        score: 40
+    }
+]
+
+// Taking the userInput
+console.log(chalk.magentaBright('Welcome to the Quiz'));
+let userName = rs.question(chalk.whiteBright("Enter your name: "));
+console.log(`${chalk.blueBright("Welcome")} ${chalk.bgMagentaBright.bold(userName)} ${chalk.greenBright("to the " + chalk.blue.underline.bold("Cricket Quiz Game."))}`);
+
+// instructions for the game
+console.log('\n');
+console.log('Rules & Instructions: ');
+console.log(`1. ${userName}, There are 10 Questions on India and all are Compulsory.`);
+console.log('2. You will get 10 points on each Right Answer.');
+console.log('3. 5 points will be deducted if the Answer is Wrong.');
+console.log('4. In MCQ based questions you have to type the Serial Number / Index Value.');
+console.log('\n');
+
+// Storing questions in the form of objects
+
+let questionOne = {
+    question: "1. Who does Goku and Krillin train with when they was kids? \n A. King Kai \n B. Master Roshi \n C.  Mr. PoPo \n D. None",
+    answer: "B"
+}
+let questionTwo = {
+    question: "2.   What is actually manga? \n A.  A form of comics  \n B. A form of animation \n C. A form of video game \n D. None",
+    answer: "B"
+}
+let questionThree = {
+    question: "3. How to run like Naruto, the main character in the anime of the same name? \n A. Put your arms back and head forward \n B. Put your right hand out, left foot in \n C. South Put your left hand out, right foot in \n D. None",
+    answer: "A"
+}
+let questionFour = {
+    question: "4. What is the meaning of Deku, the nickname of Izuku Midoriya in the anime My Hero Academia? \n A. A person who cannot do anything \n B. A nerd who loves playing game \n C.  A good friend \n D. None",
+    answer: "A"
+}
+let questionFive = {
+    question: "5. Which of the following rules is NOT true about the Death Note? \n A. Once you have written down the name of the victim and other conditions, the death will happen even when the note is destroyed \n B. You can’t kill those people who only have fewer than 5 minutes of their life left \n C. The spell will not affect a person whose name has been spelt wrongly 4 times \n D. None ",
+    answer: "B"
+}
+
+// Declaring Question array to store the value of objects
+
+let questions = [questionOne, questionTwo, questionThree, questionFour, questionFive];
+
+// Let's create a play function through which we can play the quiz game
+let play = (ques, ans) => {
+    // userInput i.e, connect the userInput inside userAnswer variable. 
+    let userAnswer = rs.question(ques);
+
+    if (userAnswer.toUpperCase() === ans.toUpperCase()) {
+        console.log(rightAnswer("\n Right Answer"));
+        score += 10;
+    } else {
+        console.log(wrongAnswer("\n Wrong Answer"));
+        console.log(chalk.green(`The correct Answer is: ${chalk.yellowBright.bold(ans)}`));
+        score -= 5;
+    }
+    if (score < 0) {
+        score = 0;
+    } else {
+        console.log("Score is: ", score);
+    }
+    console.log("\n");
+    console.log("----------------")
+    console.log(chalk.blue(`Your current score is ${chalk.yellow.bold(score)}`));
+    console.log("----------------");
+    
+}
+
+function highScorers() {
+    if (score >= 45) {
+      console.log("🔥 YAYYYYY!, You broke the record");  
+      console.log(`Congratulate ${chalk.magentaBright(userName)}, please send a screenshot so that I can update the high score list.`)
+    } else if (score >= 35 & score <=45) {
+    console.log(chalk.blueBright("Good try, you can do it better!"));
+    } else {
+      console.log(chalk.hex('#FFA500')("Aww!, better luck next time."));
+    }
+  }
+
+
+// Loop through the given question array
+for (let i = 0; i < questions.length; i++) {
+    // use play method to loop through this array
+    play(questions[i].question, questions[i].answer); 
+}
+
+console.log("High scores are: ");
+console.log("------------------")
+for (let i = 0; i < topScorers.length; i++) {
+    let currentHighScore = topScorers[i];
+    console.log(currentHighScore.name, currentHighScore.score);
+}
+
+console.log("\n");
+
+ highScorers();
